78f2469769dcec8c3a198de61e1f415b4401cb4f
Sajjad hossan <s.m.sajjadhossanshimanto.c@gmail.com>
2024-12-03 15:31:31 +0600

    wncoding fro writing html

--- fetch server.py
+++ fetch server.py
@@ -4,15 +4,15 @@ import json
 
 
 url="https://sites.google.com/view/bdixftpserverlist/live-tv-servers"
-url="https://sites.google.com/view/bdixftpserverlist/media-ftp-servers"
+# url="https://sites.google.com/view/bdixftpserverlist/media-ftp-servers"
 
 def save_web_html(url, filename):
   r = requests.get(url)
-  with open(filename, "w") as f:
+  with open(filename, "w", encoding="utf-8") as f:
     f.write(r.text)
 
 l=[]
-fs = open("active_server.txt", "a")
+fs = open("active_server.txt", "a", encoding="utf-8")
 def check_server(url):
     try:
         r = requests.get(url, timeout=3)
@@ -22,7 +22,7 @@ def check_server(url):
         return
     #print(r.status_code)
     if r.status_code==200:
-        if "facebook" in i: return 
+        if "facebook" in url: return 
         
         # data saving part
         fs.write(f"\n{url}")
@@ -31,8 +31,8 @@ def check_server(url):
         print()# to fix raw line
 
 file = "ftp_server.html"
-#save_web_html(url, file)
-with open(file) as f:
+save_web_html(url, file)
+with open(file, encoding="utf-8") as f:
     html = f.read()
 
 # p=len(html)
@@ -43,10 +43,6 @@ for link in re.finditer(pattern, html):
     check_server(p)
     c+=1
     print(".    ", c, p, end="\r")
-#    break
+    # break
 
 f.close()
-#with open("active_server.txt", "w") as f:
-#    json.dump(l, f)
- 
-
